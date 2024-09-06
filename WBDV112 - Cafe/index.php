5e2491f35e7d5b344d206fc8b0706f9235a1e503
pasebast <72749516+pasebast@users.noreply.github.com>
2024-09-07 00:50:48 +0800

    mainrepo
    mainrepo

---
+++ WBDV112 - Cafe/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Café Solstice</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Café Solstice</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">Café Solstice</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+
+        </div> 
+		
+        <div class="content">
+		
+            <h1>Welcome to<br><span>Café Solstice</span></h1>
+            <p class="par">Your favorite place for<br> the best 
+                <br> coffee and delightful treats!</p>
+
+
+                <div class="form">
+                    <h2>Login</h2>
+                    <input type="email" name="email" placeholder="Username">
+                    <input type="password" name="" placeholder="Password">
+                    <button class="btnn"><a href="#">Login</a></button>
+
+                    <p class="link">Don't have an account<br>
+                    <a href="#">Sign up </a> here</a></p>
+                    <p class="liw">Reach us through our socials:</p>
+
+                    <div class="icons">
+                        <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+             
+                    </div>
+
+                </div>
+                    </div>
+					<img src="images/coffee01.png" alt="Coffee Haven" style="width:100%; max-width:400px; height:auto;">
+                </div>
+        </div>
+    </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</body>
+</html>
+<!-- partial -->
+  
+</body>
+</html>
