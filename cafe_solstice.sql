bd83649483345dec23d23ccf16d5fd1933145d0b
pasebast <72749516+pasebast@users.noreply.github.com>
2024-11-10 01:55:52 +0800

    Add files via upload

--- cafe_solstice.sql
+++ cafe_solstice.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Nov 02, 2024 at 09:16 AM
+-- Generation Time: Nov 09, 2024 at 05:55 PM
 -- Server version: 5.0.27
 -- PHP Version: 5.2.1
 -- 
@@ -96,38 +96,43 @@ CREATE TABLE `order_items` (
   `image` varchar(255) default NULL,
   PRIMARY KEY  (`id`),
   KEY `order_id` (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=104 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=146 ;
 
 -- 
 -- Dumping data for table `order_items`
 -- 
 
 INSERT INTO `order_items` (`id`, `order_id`, `product_name`, `quantity`, `price`, `image`) VALUES 
-(79, 43, 'Sunrise Brew', 1, 80.00, 'images/menu05.png'),
-(80, 43, 'Dawn Flat White', 3, 120.00, 'images/menu06.png'),
-(81, 44, 'Eclipse Choco Delight', 1, 120.00, 'images/menu02.png'),
-(82, 44, 'Vanilla Cloud', 1, 160.00, 'images/menu08.png'),
-(83, 45, 'Eclipse Choco Delight', 1, 120.00, 'images/menu02.png'),
-(84, 46, 'Solstice Berry Bliss', 1, 150.00, 'images/menu01.png'),
-(85, 47, 'Midday Cappuccino', 1, 130.00, 'images/menu11.png'),
-(86, 47, 'Aurora Matcha Dream', 1, 190.00, 'images/menu03.png'),
-(87, 48, 'Strawberry Tart Supreme', 5, 170.00, 'images/menu18.png'),
-(88, 48, 'Sunrise Brew', 2, 80.00, 'images/menu05.png'),
-(89, 48, 'Vanilla Breeze', 1, 100.00, 'images/menu10.png'),
-(90, 48, 'Crimson Twilight Tea', 1, 110.00, 'images/menu07.png'),
-(91, 48, 'Aurora Matcha Dream', 1, 190.00, 'images/menu03.png'),
-(92, 48, 'Eclipse Choco Delight', 1, 120.00, 'images/menu02.png'),
-(93, 49, 'Eclipse Choco Delight', 2, 120.00, 'images/menu02.png'),
-(94, 50, 'Crimson Twilight Tea', 2, 110.00, 'images/menu07.png'),
-(95, 50, 'Vanilla Cloud', 3, 160.00, 'images/menu08.png'),
-(96, 50, 'Blueberry Cheesecake', 3, 160.00, 'images/menu17.png'),
-(97, 51, 'Vanilla Breeze', 2, 100.00, 'images/menu10.png'),
-(98, 51, 'Zenith Grapefruit Tea', 2, 170.00, 'images/menu09.png'),
-(99, 51, 'Stellar Soy Cocoa', 3, 170.00, 'images/menu15.png'),
-(100, 52, 'Vanilla Breeze', 1, 100.00, 'images/menu10.png'),
-(101, 52, 'Stellar Soy Cocoa', 1, 170.00, 'images/menu15.png'),
-(102, 52, 'French Toast', 3, 110.00, 'images/menu16.png'),
-(103, 52, 'Aurora Matcha Dream', 2, 190.00, 'images/menu03.png');
+(114, 57, 'Eclipse Choco Delight', 1, 120.00, 'images/menu02.png'),
+(115, 58, 'Sunrise Brew', 1, 80.00, 'images/menu05.png'),
+(118, 61, 'Solstice Berry Bliss', 1, 150.00, 'images/menu01.png'),
+(119, 61, 'Crimson Twilight Tea', 1, 110.00, 'images/menu07.png'),
+(120, 62, 'Solstice Berry Bliss', 1, 150.00, 'images/menu01.png'),
+(121, 62, 'Eclipse Choco Delight', 2, 120.00, 'images/menu02.png'),
+(122, 63, 'Spring Water', 1, 80.00, 'images/menu13.png'),
+(123, 64, 'Solstice Berry Bliss', 3, 150.00, 'images/menu01.png'),
+(124, 64, 'Eclipse Choco Delight', 2, 120.00, 'images/menu02.png'),
+(125, 64, 'Aurora Matcha Dream', 3, 190.00, 'images/menu03.png'),
+(126, 64, 'Spring Water', 1, 80.00, 'images/menu13.png'),
+(127, 65, 'Solstice Berry Bliss', 1, 150.00, 'images/menu01.png'),
+(128, 65, 'Eclipse Choco Delight', 1, 120.00, 'images/menu02.png'),
+(129, 65, 'Aurora Matcha Dream', 1, 190.00, 'images/menu03.png'),
+(130, 65, 'Solstice Shot', 1, 100.00, 'images/menu04.png'),
+(131, 65, 'Sunrise Brew', 1, 80.00, 'images/menu05.png'),
+(132, 65, 'Dawn Flat White', 1, 120.00, 'images/menu06.png'),
+(133, 65, 'Vanilla Breeze', 1, 100.00, 'images/menu10.png'),
+(134, 65, 'Midday Cappuccino', 1, 130.00, 'images/menu11.png'),
+(135, 65, 'Golden Hour Caramel', 1, 140.00, 'images/menu12.png'),
+(136, 65, 'Crimson Twilight Tea', 1, 110.00, 'images/menu07.png'),
+(137, 65, 'Vanilla Cloud', 1, 160.00, 'images/menu08.png'),
+(138, 65, 'Zenith Grapefruit Tea', 1, 170.00, 'images/menu09.png'),
+(139, 65, 'Spring Water', 1, 80.00, 'images/menu13.png'),
+(140, 65, 'Horizon Wildberry Kombucha', 1, 160.00, 'images/menu14.png'),
+(141, 65, 'Stellar Soy Cocoa', 1, 170.00, 'images/menu15.png'),
+(142, 65, 'French Toast', 1, 110.00, 'images/menu16.png'),
+(143, 65, 'Blueberry Cheesecake', 1, 160.00, 'images/menu17.png'),
+(144, 65, 'Strawberry Tart Supreme', 1, 170.00, 'images/menu18.png'),
+(145, 66, 'Classic Beef Lasagna', 1, 240.00, 'images/menu19.png');
 
 -- --------------------------------------------------------
 
@@ -146,23 +151,21 @@ CREATE TABLE `orders` (
   `cellphone_number` varchar(11) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=53 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
 
 -- 
 -- Dumping data for table `orders`
 -- 
 
 INSERT INTO `orders` (`id`, `user_id`, `order_number`, `order_date`, `total_amount`, `saved_payment`, `address`, `cellphone_number`) VALUES 
-(43, 40, 'ORD67250fb295525', '2024-11-02 01:28:18', 492.80, '1234-5555-6785-4785', 'SM City Grand Central, Rizal Avenue Extension, Barangay 88, Zone 8, Grace Park East, District 2, Caloocan, Northern Manila District, Metro Manila, 1403, Philippines', ''),
-(44, 40, 'ORD67251383c3321', '2024-11-02 01:44:35', 313.60, '1234-5555-6785-4785', 'SM City Grand Central, Rizal Avenue Extension, Barangay 88, Zone 8, Grace Park East, District 2, Caloocan, Northern Manila District, Metro Manila, 1403, Philippines', ''),
-(45, 40, 'ORD672513a5c5a27', '2024-11-02 01:45:09', 134.40, '1234-5555-6785-4785', 'SM City Grand Central, Rizal Avenue Extension, Barangay 88, Zone 8, Grace Park East, District 2, Caloocan, Northern Manila District, Metro Manila, 1403, Philippines', ''),
-(46, 40, 'ORD67251419b187b', '2024-11-02 01:47:05', 168.00, '1234-5555-6785-4785', 'SM City Grand Central, Rizal Avenue Extension, Barangay 88, Zone 8, Grace Park East, District 2, Caloocan, Northern Manila District, Metro Manila, 1403, Philippines', ''),
-(47, 1, 'ORD6725188b517ca', '2024-11-02 02:06:03', 358.40, '6969-6969-6969-6969', 'Our Lady of Fatima University, 120, MacArthur Highway, Marulas, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 1441, Philippines', ''),
-(48, 1, 'ORD672518bf8bfbe', '2024-11-02 02:06:55', 1713.60, '6969-6969-6969-6969', 'SM North Edsa, Bago Bantay, 1st District, Quezon City, Eastern Manila District, Metro Manila, Philippines', ''),
-(49, 41, 'ORD6725e61f24633', '2024-11-02 16:43:11', 268.80, '1235-5554-7824-3458', 'Our Lady of Fatima University, Tamaraw Hills Road, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 0550, Philippines', ''),
-(50, 41, 'ORD6725e7bb7826c', '2024-11-02 16:50:03', 1321.60, '1235-5554-7824-3458', 'Our Lady of Fatima University, Tamaraw Hills Road, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 0550, Philippines', '09691234565'),
-(51, 41, 'ORD6725e99e4b07d', '2024-11-02 16:58:06', 1176.00, '1235-5554-7824-3458', 'Our Lady of Fatima University, Tamaraw Hills Road, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 0550, Philippines', '09885588877'),
-(52, 1, 'ORD6725edfb6446c', '2024-11-02 17:16:43', 1097.60, '6969-6969-6969-6969', 'SM North Edsa, Bago Bantay, 1st District, Quezon City, Eastern Manila District, Metro Manila, Philippines', '09241124578');
+(57, 35, 'ORD672f760ca36d8', '2024-11-09 22:47:40', 134.40, '2024-6894-2457-1234', 'Our Lady of Fatima University, Tamaraw Hills Road, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 0550, Philippines', '09053312618'),
+(58, 35, 'ORD672f761208df1', '2024-11-09 22:47:46', 89.60, '2024-6894-2457-1234', 'Our Lady of Fatima University, Tamaraw Hills Road, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 0550, Philippines', '09053312618'),
+(61, 1, 'ORD672f78434b2c7', '2024-11-09 22:57:07', 291.20, '6969-6969-6969-6969', 'SM North Edsa, Bago Bantay, 1st District, Quezon City, Eastern Manila District, Metro Manila, Philippines', '09241124578'),
+(62, 1, 'ORD672f81f6ee92e', '2024-11-09 23:38:30', 436.80, '6969-6969-6969-6969', 'SM North Edsa, Bago Bantay, 1st District, Quezon City, Eastern Manila District, Metro Manila, Philippines', '09241124578'),
+(63, 1, 'ORD672f81fe2f6e1', '2024-11-09 23:38:38', 89.60, '6969-6969-6969-6969', 'SM North Edsa, Bago Bantay, 1st District, Quezon City, Eastern Manila District, Metro Manila, Philippines', '09241124578'),
+(64, 1, 'ORD672f835fd58eb', '2024-11-09 23:44:31', 1500.80, '6969-6969-6969-6969', 'SM North Edsa, Bago Bantay, 1st District, Quezon City, Eastern Manila District, Metro Manila, Philippines', '09241124578'),
+(65, 1, 'ORD672f83b2050d3', '2024-11-09 23:45:54', 2710.40, '6969-6969-6969-6969', 'SM North Edsa, Bago Bantay, 1st District, Quezon City, Eastern Manila District, Metro Manila, Philippines', '09241124578'),
+(66, 35, 'ORD672f8ddc3c25f', '2024-11-10 00:29:16', 268.80, '2024-6894-2457-1234', 'Our Lady of Fatima University, Tamaraw Hills Road, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 0550, Philippines', '09053312618');
 
 -- --------------------------------------------------------
 
@@ -208,6 +211,47 @@ INSERT INTO `password_resets` (`id`, `user_id`, `token`, `expires_at`) VALUES
 -- --------------------------------------------------------
 
 -- 
+-- Table structure for table `products`
+-- 
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL auto_increment,
+  `category` varchar(50) default NULL,
+  `image` varchar(255) default NULL,
+  `name` varchar(100) default NULL,
+  `description` text,
+  `price` decimal(10,2) default NULL,
+  `status` enum('active','hidden') default 'active',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+
+-- 
+-- Dumping data for table `products`
+-- 
+
+INSERT INTO `products` (`id`, `category`, `image`, `name`, `description`, `price`, `status`) VALUES 
+(1, 'frappe', 'images/menu01.png', 'Solstice Berry Bliss', 'A creamy and refreshing blend of strawberries and cream.', 150.00, 'active'),
+(2, 'frappe', 'images/menu02.png', 'Eclipse Choco Delight', 'A rich and creamy chocolate chip frappÃ©.', 120.00, 'active'),
+(3, 'frappe', 'images/menu03.png', 'Aurora Matcha Dream', 'A smooth and creamy green tea frappÃ©.', 190.00, 'active'),
+(4, 'hotcoffee', 'images/menu04.png', 'Solstice Shot', 'Strong coffee shot.', 100.00, 'active'),
+(5, 'hotcoffee', 'images/menu05.png', 'Sunrise Brew', 'Classic brewed coffee.', 80.00, 'active'),
+(6, 'hotcoffee', 'images/menu06.png', 'Dawn Flat White', 'Smooth flat white coffee.', 120.00, 'active'),
+(7, 'icedcoffee', 'images/menu10.png', 'Vanilla Breeze', 'A smooth cold brew infused with the light sweetness of vanilla cream, creating a refreshing experience as gentle as a solstice breeze.', 100.00, 'active'),
+(8, 'icedcoffee', 'images/menu11.png', 'Midday Cappuccino', 'A crisp and invigorating iced cappuccino, perfect for a refreshing boost during the peak of your day.', 130.00, 'active'),
+(9, 'icedcoffee', 'images/menu12.png', 'Golden Hour Caramel', 'A rich blend of smooth espresso, velvety milk, and sweet caramel drizzle, inspired by the golden light of the solsticeâ€™s sunset.', 140.00, 'active'),
+(10, 'iceddrinks', 'images/menu07.png', 'Crimson Twilight Tea', 'A refreshing hibiscus tea with pomegranate pearls, inspired by the deep hues of twilight.', 110.00, 'active'),
+(11, 'iceddrinks', 'images/menu08.png', 'Vanilla Cloud', 'A delightful fusion of bold black tea, coffee jelly, and smooth vanilla cold foam, this drink mirrors the soft clouds of the solstice sky.', 160.00, 'active'),
+(12, 'iceddrinks', 'images/menu09.png', 'Zenith Grapefruit Tea', 'A vibrant iced tea blend of ruby grapefruit and honey, representing the peak of refreshment during the zenith.', 170.00, 'active'),
+(13, 'bottleddrinks', 'images/menu13.png', 'Spring Water', 'Pure and refreshing, this bottled water captures the essence of a hidden spring.', 80.00, 'active'),
+(14, 'bottleddrinks', 'images/menu14.png', 'Horizon Wildberry Kombucha', 'A crisp and invigorating kombucha with wildberry essence, inspired by the endless possibilities of the horizon.', 160.00, 'active'),
+(15, 'bottleddrinks', 'images/menu15.png', 'Stellar Soy Cocoa', 'A velvety soy chocolate drink that offers a rich and indulgent experience, reminiscent of the sparkling beauty of the stars.', 170.00, 'active'),
+(16, 'pastry', 'images/menu16.png', 'French Toast', 'Bread dipped in a rich egg mixture, pan-fried to golden perfection, and topped with a sprinkle of cinnamon and maple syrup.', 110.00, 'active'),
+(17, 'pastry', 'images/menu17.png', 'Blueberry Cheesecake', 'Creamy cheesecake layered with fresh blueberries and a buttery graham cracker crust, topped with a luscious blueberry compote.', 160.00, 'active'),
+(18, 'pastry', 'images/menu18.png', 'Strawberry Tart Supreme', 'A delicate, buttery tart shell filled with smooth vanilla custard, topped with vibrant, juicy strawberries and a glossy glaze.', 170.00, 'active');
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `users`
 -- 
 
@@ -241,7 +285,6 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `addre
 (4, 'paultest3', 'paultest3@gmail.com', '670b14728ad9902aecba32e22fa4f6bd', '2024-10-12 21:17:43', '123 123 6969 madilim', '6969420069694200', '1728739086_phy12.jpg', 'Paul3', 'Test3', 'Pending', NULL, NULL, NULL, 'member', ''),
 (5, 'paultest4', 'paultest4@gmail.com', '670b14728ad9902aecba32e22fa4f6bd', '2024-10-13 03:53:14', '897234k kkk kkkk 69  69 69', '1234123412341234', '1728763128_phy25.jpg', 'Paul4', 'Test4', 'Pending', NULL, NULL, NULL, 'member', ''),
 (6, 'paultest5', 'paultest5@gmail.com', '670b14728ad9902aecba32e22fa4f6bd', '2024-10-13 04:07:47', '123 123 123 123 aaaa', '1234-1234-8888-6969', '1729865591_phy09.jpg', 'Paul5', 'Test5', 'Active', '2025-10-01', '107', NULL, 'member', ''),
-(7, 'paultest6', 'paultest6@gmail.com', '670b14728ad9902aecba32e22fa4f6bd', '2024-10-13 04:23:44', '666666 test', '1238123812381238', '', 'Paul6', 'Test6', 'Pending', NULL, NULL, NULL, 'member', ''),
 (8, 'paultest7', 'ptsebastian6585val@student.fat', '596793c886612d7387008344222dc79c', '2024-10-16 23:05:15', '123 Tamaraw Hills', '', NULL, 'Paul7', 'Test7', 'Pending', NULL, NULL, NULL, 'member', ''),
 (9, 'paultest8', 'paultest8@gmail.com', 'c4ca4238a0b923820dcc509a6f75849b', '2024-10-17 01:18:31', '123 Tamaraw Hills', '', NULL, 'Paul8', 'Test8', 'Pending', NULL, NULL, NULL, 'member', ''),
 (10, 'paultest9', 'paultest9@gmail.com', 'd77d1c8fd85502a8fe5858da6bd44446', '2024-10-17 01:20:48', '123 Tamaraw Hills', '', NULL, 'Paul9', 'Test9', 'Pending', NULL, NULL, NULL, 'member', ''),
@@ -258,7 +301,7 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `addre
 (38, 'poltest6999999', 'paultest11111111@gmail.com', 'd77d1c8fd85502a8fe5858da6bd44446', '2024-10-31 12:07:17', '6969 123 madilim', '', NULL, '              ', 'test', 'Pending', NULL, NULL, NULL, 'member', ''),
 (39, 'poltest6999999999', 'paultest11111111111@gmail.com', 'd77d1c8fd85502a8fe5858da6bd44446', '2024-10-31 12:10:23', '6969 123 madilim', '', NULL, 'paul jr.', 'test', 'Pending', NULL, NULL, NULL, 'member', ''),
 (40, 'paultest66', 'paultest66@gmail.com', '072f684e153b48e80c68bf14f3098663', '2024-11-02 01:11:57', 'SM City Grand Central, Rizal Avenue Extension, Barangay 88, Zone 8, Grace Park East, District 2, Caloocan, Northern Manila District, Metro Manila, 1403, Philippines', '1234-5555-6785-4785', '1730481486_rys01.jpg', 'paul gerald', 'sebastian', 'Active', '2028-10-01', '443', NULL, 'member', ''),
-(41, 'paultest55', 'ptsebastian6585val@student.fatima.edu.ph', '072f684e153b48e80c68bf14f3098663', '2024-11-02 16:18:37', 'Our Lady of Fatima University, Tamaraw Hills Road, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 0550, Philippines', '1235-5554-7824-3458', '1730536749_phy41.jpg', 'Paul Pinas', 'Testing', 'Active', '2027-11-01', '743', NULL, 'member', '09885588877');
+(41, 'paultest55', 'ptsebastian6585val@student.fatima.edu.ph', '072f684e153b48e80c68bf14f3098663', '2024-11-02 16:18:37', 'OLFU RISE Tower, Fatima Avenue, Deato Subdivision, 2nd District, Valenzuela, Northern Manila District, Metro Manila, 1441, Philippines', '1235-5554-7824-3458', '1730536749_phy41.jpg', 'Paul Test', 'Sebastian', 'Active', '2027-11-01', '743', NULL, 'member', '09885588877');
 
 -- 
 -- Constraints for dumped tables
